bf7b566b42362dfdaa5922dedd401cfc25f09d33
Dane Mackier <dane.mackier@outlook.com>
2025-11-27 15:42:06 +0200

    Initial Commit

---
+++ CourseViewerDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CourseViewerDemo
+//
+//  Created by Dane Mackier on 2025/11/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CourseViewerDemoTests/CourseViewerDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  CourseViewerDemoTests.swift
+//  CourseViewerDemoTests
+//
+//  Created by Dane Mackier on 2025/11/27.
+//
+
+import Testing
+@testable import CourseViewerDemo
+
+struct CourseViewerDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
