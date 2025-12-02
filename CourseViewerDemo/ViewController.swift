ff3dac9524e85ecd7564d2ba60be37ef61c75165
Dane Mackier <dane.mackier@outlook.com>
2025-12-02 12:11:46 +0200

    initial commit [not working]

--- CourseViewerDemo/ViewController.swift
+++ CourseViewerDemo/ViewController.swift
@@ -1,19 +1,39 @@
-//
-//  ViewController.swift
-//  CourseViewerDemo
-//
-//  Created by Dane Mackier on 2025/11/27.
-//
-
 import UIKit
+import IGolfViewer3D
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var renderView: CourseRenderView!
+    
+    private var isRenderViewLoaded = false
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        loadGolfCourse()
     }
-
-
+    
+    
+    private func loadGolfCourse() {
+        
+        guard !isRenderViewLoaded else {
+            return
+        }
+        
+        isRenderViewLoaded = true
+        
+        let loader = CourseRenderViewLoader(
+            applicationAPIKey: "gAil-pspH8q4PgM",
+            applicationSecretKey: "CgkDhdiAZoocUWf135VlKc4BGd2xrq",
+            idCourse: "fAwbKaonIp7Q")
+        
+        let activityIndicatorView = UIActivityIndicatorView(style: .large)
+        
+        loader.setLoading(activityIndicatorView)
+        loader.initialNavigationMode = .modeOverallHole
+        
+        renderView.load(with: loader)
+        
+        activityIndicatorView.startAnimating()
+    }
 }
-
